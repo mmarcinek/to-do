ccfc0d6eb450277a75d475762725906094b0ea40
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-05 10:59:15 -0400

    Form completes Todo and appends HTML

--- app/js/main.js
+++ app/js/main.js
@@ -2,18 +2,22 @@
 // create constructor function for Todo:
 var Todo = function (options) {
   var args = options || {};
-  this.text = options.text;
-  this.status = options.status;
+  this.text = args.text;
+  this.status = args.status;
 };
 
+
+
 //Connect input field and button event to Todo:
-$('.textTodo').val()
+$('.addDo').on('click', function (e) {
+  e.preventDefault();
+
+  var x = $('textDo').val();
+  var b = new Todo({text: $('.textDo').val(), status: 'Open'});
+
+  $('.list').append('<li>' + b.text + '</li>');
+
+  console.log(b);
+});
 
 
-
-// sandbox:
-var todo1 = new Todo({text: 'sell car', status: 'open'});
-// var todo2 = new Todo({text: 'buy eggs'});
-
-
-// })();
