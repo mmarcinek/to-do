a72a9d498bd3972070459f32e0cd2391c84eab6b
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-05 17:54:28 -0400

    Makes a mess of class swap and property target. Needs to only target task that is being completed, swaps on everything in array.

--- app/js/main.js
+++ app/js/main.js
@@ -7,23 +7,51 @@ var Todo = function (options) {
 };
 
 var openTasks = [];
+var closedTasks = [];
+
 
 //Connect input field and button event to Todo:
-$('.addDo').on('click', function (e) {
+$('.input').on('submit', function (e) {
   e.preventDefault();
 
   var x = $('textDo').val();
   var task = new Todo({text: $('.textDo').val(), status: 'Open'});
 
+// push tasks to array openTasks
   openTasks.push(task);
 
-  $('.list').append('<li>' + task.text + '</li>');
+// appends HTML list item
+  $('ul').append('<li class="taskList">' + task.text + '</li>');
 
+// resets input
   this.reset();
 
+  console.log(task);
 
 
-  console.log(b);
+// moves openTasks list item to closedTasks array when clicked
+
+  $('#tasks').on('click', function (event) {
+    $(this).toggleClass('strike');
+    var taskDone = $(this).text();
+    var taskToEdit = _.find(openTasks, {task: taskDone});
+//     taskToEdit.status = 'Closed';
+    console.log(openTasks);
+  });
+
+
+  // $('.taskList').on('click', function (event) {
+  //   closedTasks.push(task);
+  // });
+
 });
+
+// $('#tasks').on('click', 'li', function() {
+//  event.preventDefault();
+//  $(this).toggleClass('complete');
+//  var tTask = $(this).text();
+//  var taskToEdit = _.find(storage, {task: tTask});
+//  taskToEdit.status = 'Complete';
+// });
 
 
